a7151c8aa9d714ea7c9e2c335850e5ae35d57421
jredfox <jredfox_@outlook.com>
2024-01-10 00:54:21 -0600

    ADD Java GPU

--- src/GameMode.bat
+++ src/GameMode.bat
@@ -5,7 +5,7 @@ reg add "HKCU\Software\Microsoft\GameBar" /v AllowAutoGameMode /t REG_DWORD /d 1
 reg add "HKCU\Software\Microsoft\GameBar" /v AutoGameModeEnabled /t REG_DWORD /d 1 /f
 REM ## Enable GPU Schedualing ##
 reg add "HKLM\SYSTEM\CurrentControlSet\Control\GraphicsDrivers" /v HwSchMode /t REG_DWORD /d 2 /f
-REM ## Enable HDR Settings WIP ##
+REM ## Enable HDR Settings ##
 reg add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\HDR" /v "UseHDR" /t REG_DWORD /d 1 /f
 reg add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\HDR" /v "AllowHDRForGames" /t REG_DWORD /d 1 /f
 reg add "HKCU\System\GameConfigStore" /v "GameDVR_StreamingEnabled" /t REG_DWORD /d 1 /f
@@ -21,6 +21,11 @@ reg add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\VideoSettings" /v "Disab
 REM ## Enable HDR On Battery ##
 reg add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\VideoSettings" /v "DisableHDROnBattery" /t REG_DWORD /d 0 /f
 reg add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\VideoSettings" /v "DisableHDRSupportOnBattery" /t REG_DWORD /d 0 /f
+REM ## Enable GPU Performance on Java and Python ##
+call :ADDGPU "java.exe"
+call :ADDGPU "javaw.exe"
+call :ADDGPU "py.exe"
+call :ADDGPU "pyw.exe"
 
 REM ################################### START DISABLING ANOYING THINGS ####################################
 REM ## Disable Sticky Keys REQUIRES Logging Off and Back On ##
@@ -33,4 +38,14 @@ REM ## VALUES = SwapEffectUpgradeEnable=1;AutoHDREnable=1;VRROptimizeEnable=1;
 FOR /F "tokens=3*" %%a in ('reg query "HKEY_CURRENT_USER\SOFTWARE\Microsoft\DirectX\UserGpuPreferences" /v "DirectXUserGlobalSettings" 2^>nul') DO (set VRR=%%a)
 echo !VRR!
 pause
+exit /b
+
+:ADDGPU
+set exe=%~1
+FOR /F "delims=" %%I IN ('where "!exe!"') DO (
+reg query HKCU\SOFTWARE\Microsoft\DirectX\UserGpuPreferences /v "%%I" >nul 2>&1
+IF !ERRORLEVEL! NEQ 0 (
+reg add "HKCU\SOFTWARE\Microsoft\DirectX\UserGpuPreferences" /v "%%I" /t REG_SZ /d "GpuPreference=2;" /f >nul 2>&1
+)
+)
 exit /b
