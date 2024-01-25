4024004066e9c2fd1fa901c7261619e0935569ad
jredfox <jredfox_@outlook.com>
2024-01-24 21:00:38 -0600

    Finish Main

--- src/Install.bat
+++ src/Install.bat
@@ -7,14 +7,10 @@ REM ## Start Main Installation ##
 set gmset=%~1
 IF /I "%gmset:~0,1%" NEQ "T" (GOTO MAIN)
 echo Installing GameModeLib Main Settings
+call "%~dp0Executables\PowerModeOverlay.exe" "ded574b5-45a0-4f42-8737-46345c09c238"
 reg import "%~dp0Main.reg"
 REM ## Create GameMode Power Plan and Enable Dedicated Graphics for Java and Python ##
-call "%~dp0GameModeLib.exe" -PowerPlan -GPUEntry "java.exe;javaw.exe;py.exe;pyw.exe"
-REM ## Fix Power Plan Performance ##
-set BestPP=ded574b5-45a0-4f42-8737-46345c09c238
-call "%~dp0Executables\PowerModeOverlay.exe" "!BestPP!"
-reg add "HKLM\SYSTEM\CurrentControlSet\Control\Power\User\PowerSchemes" /v "ActiveOverlayAcPowerScheme" /t REG_SZ /d "!BestPP!" /f
-reg add "HKLM\SYSTEM\CurrentControlSet\Control\Power\User\PowerSchemes" /v "ActiveOverlayDcPowerScheme" /t REG_SZ /d "!BestPP!" /f
+call "%~dp0GameModeLib.exe" -GPUEntry "java.exe;javaw.exe;py.exe;pyw.exe" -PowerPlan -SetPowerPlan
 :MAIN
 
 :END
