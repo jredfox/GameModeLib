4892ddb3a2391f8daef00f84c8ee742b6893bf1c
jredfox <jredfox_@outlook.com>
2024-01-28 00:18:23 -0600

    fix script not working
    fix weird behavior of script not working. I have to call echo "msg" >nul 2>&1 in order to initialize something or windows reverts real time protection and it's other settings to back on

--- src/Executables/WDStaticDisable.bat
+++ src/Executables/WDStaticDisable.bat
@@ -1,6 +1,7 @@
 @ECHO OFF
 setlocal enableDelayedExpansion
+echo init >nul 2>&1
 REM ## Disables Windows Defender Notfications ##
-reg import "%~dp0WDDisable.reg"
+reg import "%~dp0WDDisable.reg" >nul 2>&1
 REM ## Disable Windows Defender Except Firewall ##
-powershell -ExecutionPolicy Bypass -File "%~dp0WDDisable.ps1"
+powershell -ExecutionPolicy Bypass -File "%~dp0WDDisable.ps1" >nul 2>&1
