434114f5de4b2ad8b27132dd12a9ecb67e08f137
jredfox <jredfox_@outlook.com>
2024-01-28 23:40:20 -0600

    Fool Old Windows 10
    fool old windows 10 into thinking the registry edit was done by the user thus preventing an undoing of the change on the next reboot

--- src/Executables/WDStaticDisable.bat
+++ src/Executables/WDStaticDisable.bat
@@ -3,5 +3,6 @@ setlocal enableDelayedExpansion
 echo init >nul 2>&1
 REM ## Disables Windows Defender Notfications ##
 reg import "%~dp0WDDisable.reg" >nul 2>&1
+regedit /S "%~dp0WDDisable.reg" >nul 2>&1
 REM ## Disable Windows Defender Except Firewall ##
 powershell -ExecutionPolicy Bypass -File "%~dp0WDDisable.ps1" >nul 2>&1
