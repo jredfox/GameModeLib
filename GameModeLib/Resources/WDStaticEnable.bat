3eac230925fbe3b44833c2ee2ed3c1fd0438aabb
jredfox <jredfox_@outlook.com>
2024-02-26 09:40:01 -0600

    Update WDStaticEnable.bat

--- GameModeLib/Resources/WDStaticEnable.bat
+++ GameModeLib/Resources/WDStaticEnable.bat
@@ -2,4 +2,5 @@
 setlocal enableDelayedExpansion
 REM ## Enables Windows Defender Notfications ##
 reg import "%~dp0WDEnable.reg"
+regedit /S "%~dp0WDEnable.reg"
 powershell -ExecutionPolicy Bypass -File "%~dp0WDEnable.ps1"
