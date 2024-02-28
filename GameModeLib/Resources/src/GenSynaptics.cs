5dff6a965a8cdfa4b47f7753badab96af7d8d4c6
jredfox <jredfox_@outlook.com>
2024-02-27 21:05:06 -0600

    GenSynaptics.exe
    GenSynaptics which generates User Synaptics reg file dynamically

---
+++ GameModeLib/Resources/src/GenSynaptics.cs
@@ -0,0 +1,73 @@
+using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenSynaptics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Get Absolute File and add Extension if Required
+            string str_file = Path.GetFullPath(args[0]);
+            if(!str_file.ToUpper().EndsWith(".REG"))
+            {
+                str_file += ".reg";
+            }
+            //Create Dir if it doesn't exist
+            string dir = Path.GetDirectoryName(str_file);
+            if (!Directory.Exists(dir))
+            {
+                Directory.CreateDirectory(dir);
+            }
+            //Cleanup
+            if (File.Exists(str_file))
+                File.Delete(str_file);
+
+            using (StreamWriter writer = new StreamWriter(str_file))
+            {
+                RegistryKey tree = RegistryKey.OpenBaseKey(RegistryHive.CurrentUser, Environment.Is64BitOperatingSystem ? RegistryView.Registry64 : RegistryView.Default);
+                RegistryKey key_syntp = tree.OpenSubKey(@"SOFTWARE\Synaptics\SynTP", false);
+                if (key_syntp == null)
+                {
+                    Console.WriteLine(@"Synaptics\SynTP Not Found");
+                    return;
+                }
+                writer.WriteLine("Windows Registry Editor Version 5.00");
+                foreach (var device in key_syntp.GetSubKeyNames())
+                {
+                    try
+                    {
+                        RegistryKey key_device = key_syntp.OpenSubKey(device);
+                        writer.WriteLine("\r\n[" + key_device.Name + "]");
+                        writer.WriteLine("\"PalmDetectConfig\"=dword:00000000");
+                        writer.WriteLine("\"PalmRejectAlways\"=dword:00000000");
+                        writer.WriteLine("\"PalmRT\"=dword:00000000");
+                        Close(key_device);
+                    }
+                    catch(Exception e)
+                    {
+                        Console.Error.WriteLine(e);
+                    }
+                }
+                Close(key_syntp);
+            }
+        }
+        public static void Close(RegistryKey key)
+        {
+            try
+            {
+                if (key != null)
+                    key.Close();
+            }
+            catch (Exception e)
+            {
+                Console.Error.WriteLine(e);
+            }
+        }
+    }
+}
