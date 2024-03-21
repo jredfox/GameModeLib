8ea60473682df568d6db9040c8f38b3a8ab94c92
jredfox <jredfox_@outlook.com>
2024-03-21 17:00:21 -0500

    DevDrive Support
    Added Windows 11 Dev Drive Support which actually suppose to increase performance when realtime protection is on. So We need this enabled for our WDLowCPU module

--- GameModeLib/Resources/WDEnableLowCPU.ps1
+++ GameModeLib/Resources/WDEnableLowCPU.ps1
@@ -1,3 +1,4 @@
 $ErrorActionPreference = 'SilentlyContinue'
 Set-MpPreference -Force -EnableLowCpuPriority $true
 Set-MpPreference -Force -ScanAvgCPULoadFactor 15
+set-MpPreference -Force -PerformanceModeStatus Enabled
--- GameModeLib/Resources/WDSetLowCPU.ps1
+++ GameModeLib/Resources/WDSetLowCPU.ps1
@@ -1,14 +1,22 @@
 param(
 [Parameter(Mandatory=$true)][string]$EnableLowCPU,
-[Parameter(Mandatory=$true)][string]$ScanAvg
+[Parameter(Mandatory=$true)][string]$ScanAvg,
+[Parameter(Mandatory=$true)][string]$PerfDrive
 )
-if($EnableLowCPU -eq "TRUE") {
+if ($EnableLowCPU -eq "TRUE") {
     $LowCPU = $true
 }
 else {
     $LowCPU = $false
 }
+if ($PerfDrive -eq "1") {
+    $PerfDrive = "Enabled"
+}
+elseif ($PerfDrive -eq "0") {
+    $PerfDrive = "Disabled"
+}
 
 $ErrorActionPreference = 'SilentlyContinue'
 Set-MpPreference -Force -EnableLowCpuPriority $LowCPU
 Set-MpPreference -Force -ScanAvgCPULoadFactor $ScanAvg
+Set-MpPreference -Force -PerformanceModeStatus $PerfDrive
