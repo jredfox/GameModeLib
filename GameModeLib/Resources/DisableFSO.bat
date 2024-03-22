eb74f13b9dbfb6e1251325a881d7a678fc8f032c
jredfox <jredfox_@outlook.com>
2024-03-22 18:58:02 -0500

    Improve WorkAround
    sometimes windows didn't overwrite the FSO registry keys till after they were messed with so I am messing with them on both launches to garentee 98%(windows bug itself) of the time it will work if they user fallows instructions

--- GameModeLib/Resources/DisableFSO.bat
+++ GameModeLib/Resources/DisableFSO.bat
@@ -1,5 +1,11 @@
 @ECHO OFF
 setlocal enableDelayedExpansion
+reg add "HKEY_CURRENT_USER\System\GameConfigStore" /v "GameDVR_FSEBehavior" /t REG_DWORD /d 2 /f
+reg add "HKEY_CURRENT_USER\System\GameConfigStore" /v "GameDVR_FSEBehaviorMode" /t REG_DWORD /d 2 /f
+reg add "HKEY_CURRENT_USER\System\GameConfigStore" /v "GameDVR_HonorUserFSEBehaviorMode" /t REG_DWORD /d 1 /f
+reg add "HKEY_CURRENT_USER\System\GameConfigStore" /v "GameDVR_DXGIHonorFSEWindowsCompatible" /t REG_DWORD /d 1 /f
+reg add "HKEY_CURRENT_USER\System\GameConfigStore" /v "GameDVR_EFSEFeatureFlags" /t REG_DWORD /d 0 /f
+reg add "HKEY_CURRENT_USER\System\GameConfigStore" /v "GameDVR_DSEBehavior" /t REG_DWORD /d 2 /f
 set file=%USERPROFILE%\DisableFSO
 IF NOT EXIST "%file%" (
 echo FirstRun >"%file%"
@@ -7,13 +13,6 @@ echo Please Re-Login or Reboot to Disable Full Screen Optimizations
 pause
 exit /b
 )
-
-reg add "HKEY_CURRENT_USER\System\GameConfigStore" /v "GameDVR_FSEBehavior" /t REG_DWORD /d 2 /f
-reg add "HKEY_CURRENT_USER\System\GameConfigStore" /v "GameDVR_FSEBehaviorMode" /t REG_DWORD /d 2 /f
-reg add "HKEY_CURRENT_USER\System\GameConfigStore" /v "GameDVR_HonorUserFSEBehaviorMode" /t REG_DWORD /d 1 /f
-reg add "HKEY_CURRENT_USER\System\GameConfigStore" /v "GameDVR_DXGIHonorFSEWindowsCompatible" /t REG_DWORD /d 1 /f
-reg add "HKEY_CURRENT_USER\System\GameConfigStore" /v "GameDVR_EFSEFeatureFlags" /t REG_DWORD /d 0 /f
-reg add "HKEY_CURRENT_USER\System\GameConfigStore" /v "GameDVR_DSEBehavior" /t REG_DWORD /d 2 /f
 reg delete "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Run" /v "GMLDisableFSO" /f
 echo Full Screen Optimizations Disabled. Please Re-Login Reboot For it to Take Effect
 pause
