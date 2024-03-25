311f158c36d0df45cb99a055f0af2e27f3c2bd40
jredfox <jredfox_@outlook.com>
2024-03-24 19:03:45 -0500

    Fix Derp

--- GameModeLib/Install.bat
+++ GameModeLib/Install.bat
@@ -102,7 +102,9 @@ IF /I "!Settings:~1,1!" NEQ "T" (GOTO GRAPHICS)
 ::AMD 3D Graphics Settings
 start /MIN cmd /c call "!rc!\AMD3dSettings.exe" "!ugen!" ^>"!log_graphics!" ^2^>^&1
 ::NVIDIA 3D Graphics Settings Set Preffered Graphics Processor to High Performance
-start /MIN cmd /c call "!rc!\NVIDIA3DSettings.exe" ^>"!ugen!\NVIDIA.txt" ^2^>"!log_nvidia!"
+set nvfile=!ugen!\NVIDIA.txt
+IF EXIST "!nvfile!" (GOTO GRAPHICS)
+start /MIN cmd /c call "!rc!\NVIDIA3DSettings.exe" ^>"!nvfile!" ^2^>"!log_nvidia!"
 :GRAPHICS
 
 REM ## Update Sticky Keys ##
