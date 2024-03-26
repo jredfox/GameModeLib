68c0c72741adcab9bb5071490ba0af994b341024
jredfox <jredfox_@outlook.com>
2024-03-26 01:44:41 -0500

    Fix DERP

--- GameModeLib/Resources/NVIDIAInstall.bat
+++ GameModeLib/Resources/NVIDIAInstall.bat
@@ -6,10 +6,10 @@ set log_nvidia=%~2
 IF /I "%~3" EQU "T" (set NVARGS=/NoVSYNC /NoPWR) ELSE (set "NVARGS=")
 REM ## Install NVIDIA Preffered Graphics Processor to High Performance and Gen Uninstall Data ##
 IF EXIST "!nvfile!" (
-call "!rc!\NVIDIA3DSettings.exe" !NVARGS! >nul 2>"!log_nvidia!"
+call "%~dp0NVIDIA3DSettings.exe" !NVARGS! >nul 2>"!log_nvidia!"
 exit /b
 )
-call "!rc!\NVIDIA3DSettings.exe" !NVARGS! >"!nvfile!" 2>"!log_nvidia!"
+call "%~dp0NVIDIA3DSettings.exe" !NVARGS! >"!nvfile!" 2>"!log_nvidia!"
 REM ## Delete Blank NVIDIA.txt ##
 call :ISBLANK "!nvfile!"
 IF "!isBlank!" EQU "T" (del /F /Q /A "!nvfile!" >nul 2>&1)
