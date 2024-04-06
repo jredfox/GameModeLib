03b4519e0d6fe547b1b9a2bf8504162aa71d2681
jredfox <jredfox_@outlook.com>
2024-04-06 16:46:40 -0500

    Update RestoreDefaults.bat

--- GameModeLib/RestoreDefaults.bat
+++ GameModeLib/RestoreDefaults.bat
@@ -95,6 +95,7 @@ IF /I "!Settings:~9,1!" NEQ "T" (GOTO NVPP)
 taskkill /F /FI "IMAGENAME eq GameModeLibNVPP*"
 schtasks /DELETE /tn "GameModeLibNVPP" /F
 set GameModeLibNVPP=
+IF /I "!Settings:~1,1!" NEQ "T" (call "!rc!\NVIDIA3DSettings.exe" import "0x00A879CF=Default;0x1057EB71=Default;0x10F9DC80=Default;0x10F9DC81=Default;0x10F9DC84=Default" >nul 2>&1)
 call "!rc!\GameModeLibNVPP-CLI.exe" "/Uninstall" >nul 2>&1
 REM Del Old Files
 del /F /Q /A "!PROGRAMFILES!\GameModeLib\GameModeLibNVPP.exe" >nul 2>&1
